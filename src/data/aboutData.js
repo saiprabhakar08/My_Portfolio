d3e7c0299d041ae1dc6ab982e8e084f727fad04d
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-03-22 10:29:45 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -4,7 +4,7 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name's Venkata Sai Prabhakar Palakurthi. I am a dedicated Front-End Web Developer with expertise in HTML, CSS, JavaScript, and React.",
-    description2: "I am also proficient in programming languages like C, C++, and Python, with a solid understanding of Data Structures and Algorithms. My skills allows me to develop responsive web applications with efficiency.",
+    description2: "I am also proficient in programming languages like C, C++, and Java, with a solid understanding of Data Structures and Algorithms. My skills allows me to develop responsive web applications with efficiency.",
     image: 1
 };
 
