9c49f16c6215a328149115679b3f5c13b0a58e9d
SaiPrabhakar08 <162802761+saiprabhakar08@users.noreply.github.com>
2025-08-26 18:26:40 +0530

    Update achievement data and add new certifications

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,6 +1,9 @@
 import redhat from '../assets/svg/projects/redhat.png'
 import html from '../assets/svg/projects/html.png'
-import oracle from '../assets/svg/projects/oracle.png'
+import oracle from '../assets/svg/projects/oracle.PNG'
+import javascript from '../assets/svg/projects/javascript.PNG'
+import servicenowcsa from '../assets/svg/projects/servicenowcsa.PNG'
+import servicenowcad from '../assets/svg/projects/servicenowcad.PNG'
 import java from '../assets/svg/projects/java.png'
 import python from '../assets/svg/projects/python.png'
 import My_Java from '../assets/svg/projects/My_Java.PNG'
@@ -23,33 +26,50 @@ export const achievementData = {
             title: 'HTML & CSS',
             details: 'Demonstrates expertise in building structured, responsive, and visually appealing web pages with modern styling techniques.',
             date: 'Dec 16, 2022',
-            field: 'WebDevelopment',
+            field: 'Web Development',
             image: My_Html_Css
         },
-        // {
-        //     id: 3,
-        //     title: 'Database Foundations',
-        //     details: 'Certifies skills in SQL, relational database concepts, data retrieval, and database management fundamentals.',
-        //     date: 'May 17, 2024',
-        //     field: 'Programming',
-        //     image: oracle
-        // },
+        {
+            id: 3,
+            title: 'Oracle Certified Foundations Associate',
+            details: 'Certifies skills in SQL, relational database concepts, data retrieval, and database management fundamentals.',
+            date: 'July 16, 2025',
+            field: 'Database',
+            image: oracle
+        },
         {
             id: 4,
             title: 'Java',
-            details: 'Covers programming fundamentals, data structures, automation, and problem-solving with Python.',
+            details: 'Covers programming fundamentals, data structures, automation, and problem-solving with Java.',
             date: 'July 8, 2024',
-            field: 'Development',
+            field: 'Programming',
             image: My_Java
         },
-        // {
-        //     id: 5,
-        //     title: 'Python',
-        //     details: 'Red Hat certification validates your skills in system administration, security, and automation with Red Hat technologies.',
-        //     date: 'May 11, 2023',
-        //     field: 'Database',
-        //     image: python
-        // }
+        {
+            id: 5,
+            title: 'JavaScript',
+            details: 'Demonstrates proficiency in developing dynamic, interactive, and efficient web applications using core JavaScript concepts and modern programming practices.',
+            date: 'June 29, 2025',
+            field: 'Development',
+            image: javascript
+        },
+        {
+            id: 6,
+            title: 'ServiceNow CSA',
+            details: 'Validates expertise in configuring, managing, and maintaining ServiceNow instances to streamline digital workflows and enhance business processes.',
+            date: 'May 22, 2025',
+            field: 'ITSM Tool',
+            image: servicenowcsa
+        },
+        {
+            id: 7,
+            title: 'ServiceNow CAD',
+            details: 'Validates skills in designing, developing, and implementing custom applications on the ServiceNow platform using best practices and modern development approaches.',
+            date: 'May 30, 2025',
+            field: 'ITSM Tool',
+            image: servicenowcad
+        },
+        
     ]
 };
 
